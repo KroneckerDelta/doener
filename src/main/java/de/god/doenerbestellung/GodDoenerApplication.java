c90c4f3c0d9f1d9c6e51df540237847f3c17cac5
Thomas Michael <thomas.michael@god.de>
2017-06-22 22:03:22 +0200

    ascii art

--- src/main/java/de/god/doenerbestellung/GodDoenerApplication.java
+++ src/main/java/de/god/doenerbestellung/GodDoenerApplication.java
@@ -5,12 +5,38 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/***
+ * <pre>
+ *     ____     ___    ____ __  __  ____ ____      ___  ____  ____ 
+ *     || \\   // \\  ||    ||\ || ||    || \\    // \\ || \\ || \\
+ *     ||  )) ((   )) ||==  ||\\|| ||==  ||_//    ||=|| ||_// ||_//
+ *     ||_//   \\_//  ||___ || \|| ||___ || \\    || || ||    ||
+ * 
+ * </pre>
+ * 
+ * @author tmichael
+ *
+ */
 @ComponentScan
 @Configuration
 @EnableAutoConfiguration
 public class GodDoenerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Starting App:");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("██████╗  ██████╗ ███████╗███╗   ██╗███████╗██████╗      █████╗ ██████╗ ██████╗ ");
+		System.out.println("██╔══██╗██╔═══██╗██╔════╝████╗  ██║██╔════╝██╔══██╗    ██╔══██╗██╔══██╗██╔══██╗");
+		System.out.println("██║  ██║██║   ██║█████╗  ██╔██╗ ██║█████╗  ██████╔╝    ███████║██████╔╝██████╔╝");
+		System.out.println("██║  ██║██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗    ██╔══██║██╔═══╝ ██╔═══╝ ");
+		System.out.println("██████╔╝╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║    ██║  ██║██║     ██║     ");
+		System.out.println("╚═════╝  ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝     ╚═╝     ");
+		System.out.println("");
+		System.out.println("");
+
 		SpringApplication.run(GodDoenerApplication.class, args);
 	}
 }
