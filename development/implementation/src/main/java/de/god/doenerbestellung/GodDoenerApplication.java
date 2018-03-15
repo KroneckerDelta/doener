2a3051150c3becc27a2a22b795571e53fca844f4
Thomas Michael <thomas.michael@god.de>
2018-03-15 22:01:35 +0100

    Bessere Loggin beim Start

--- development/implementation/src/main/java/de/god/doenerbestellung/GodDoenerApplication.java
+++ development/implementation/src/main/java/de/god/doenerbestellung/GodDoenerApplication.java
@@ -29,47 +29,76 @@ public class GodDoenerApplication {
 			System.out.println("Starting App:");
 			System.out.println("");
 			System.out.println("");
-			System.out.println("██████╗  ██████╗ ███████╗███╗   ██╗███████╗██████╗      █████╗ ██████╗ ██████╗ ");
-			System.out.println("██╔══██╗██╔═══██╗██╔════╝████╗  ██║██╔════╝██╔══██╗    ██╔══██╗██╔══██╗██╔══██╗");
-			System.out.println("██║  ██║██║   ██║█████╗  ██╔██╗ ██║█████╗  ██████╔╝    ███████║██████╔╝██████╔╝");
-			System.out.println("██║  ██║██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗    ██╔══██║██╔═══╝ ██╔═══╝ ");
-			System.out.println("██████╔╝╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║    ██║  ██║██║     ██║     ");
-			System.out.println("╚═════╝  ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝     ╚═╝     ");
-			System.out.println("");
-			System.out.println("");
+			System.out.println("//  O)))))                                                               O)                       ");
+			System.out.println("//  O))   O))                                                           O) ))                     ");
+			System.out.println("//  O))    O))   O))       O))    O)) O))     O))    O) O)))           O)  O))    O) O))  O) O))  ");
+			System.out.println("//  O))    O)) O))  O))  O)   O))  O))  O)) O)   O))  O))             O))   O))   O)  O)) O)  O)) ");
+			System.out.println("//  O))    O))O))    O))O))))) O)) O))  O))O))))) O)) O))            O)))))) O))  O)   O))O)   O))");
+			System.out.println("//  O))   O))  O))  O)) O)         O))  O))O)         O))           O))       O)) O)) O)) O)) O)) ");
+			System.out.println("//  O)))))       O))      O))))   O)))  O))  O))))   O)))          O))         O))O))     O))     ");
+			System.out.println("//                                                                                O))     O))     ");
+
 			try {
 				SpringApplication.run(GodDoenerApplication.class, args);
 				System.out.println("                                                                                   ");
-				System.out.println("██████╗  ██████╗ ███████╗███╗   ██╗███████╗██████╗     ██████╗ ██╗   ██╗███╗   ██╗");
-				System.out.println("██╔══██╗██╔═══██╗██╔════╝████╗  ██║██╔════╝██╔══██╗    ██╔══██╗██║   ██║████╗  ██║");
-				System.out.println("██║  ██║██║   ██║█████╗  ██╔██╗ ██║█████╗  ██████╔╝    ██████╔╝██║   ██║██╔██╗ ██║");
-				System.out.println("██║  ██║██║   ██║██╔══╝  ██║╚██╗██║██╔══╝  ██╔══██╗    ██╔══██╗██║   ██║██║╚██╗██║");
-				System.out.println("██████╔╝╚██████╔╝███████╗██║ ╚████║███████╗██║  ██║    ██║  ██║╚██████╔╝██║ ╚████║");
-				System.out.println("╚═════╝  ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝    ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝");
+				System.out.println("//  O)))))                                                         O)))))))                     ");
+				System.out.println("//  O))   O))                                                      O))    O))                   ");
+				System.out.println("//  O))    O))   O))       O))    O)) O))     O))    O) O)))       O))    O))  O))  O))O)) O))  ");
+				System.out.println("//  O))    O)) O))  O))  O)   O))  O))  O)) O)   O))  O))          O) O))      O))  O)) O))  O))");
+				System.out.println("//  O))    O))O))    O))O))))) O)) O))  O))O))))) O)) O))          O))  O))    O))  O)) O))  O))");
+				System.out.println("//  O))   O))  O))  O)) O)         O))  O))O)         O))          O))    O))  O))  O)) O))  O))");
+				System.out.println("//  O)))))       O))      O))))   O)))  O))  O))))   O)))          O))      O))  O))O))O)))  O))");
+				System.out.println("//                                                                                              ");
 				System.out.println("                                                                                   ");
 				break;
 
 			} catch (Exception e) {
 
 				System.out.println("                                                                                   ");
-				System.out.println("████████╗██████╗ ██╗   ██╗     █████╗  ██████╗  █████╗ ██╗███╗   ██╗");
-				System.out.println("╚══██╔══╝██╔══██╗╚██╗ ██╔╝    ██╔══██╗██╔════╝ ██╔══██╗██║████╗  ██║");
-				System.out.println("   ██║   ██████╔╝ ╚████╔╝     ███████║██║  ███╗███████║██║██╔██╗ ██║");
-				System.out.println("   ██║   ██╔══██╗  ╚██╔╝      ██╔══██║██║   ██║██╔══██║██║██║╚██╗██║");
-				System.out.println("   ██║   ██║  ██║   ██║       ██║  ██║╚██████╔╝██║  ██║██║██║ ╚████║");
-				System.out.println("   ╚═╝   ╚═╝  ╚═╝   ╚═╝       ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝");
+				System.out.println("//                                                                                                                                                       ");
+				System.out.println("//                                                                                                                                                       ");
+				System.out.println("//  TTTTTTTTTTTTTTTTTTTTTTT                                                                                                       iiii                   ");
+				System.out.println("//  T:::::::::::::::::::::T                                                                                                      i::::i                  ");
+				System.out.println("//  T:::::::::::::::::::::T                                                                                                       iiii                   ");
+				System.out.println("//  T:::::TT:::::::TT:::::T                                                                                                                              ");
+				System.out.println("//  TTTTTT  T:::::T  TTTTTTrrrrr   rrrrrrrrryyyyyyy           yyyyyyy       aaaaaaaaaaaaa     ggggggggg   ggggg aaaaaaaaaaaaa   iiiiiiinnnn  nnnnnnnn    ");
+				System.out.println("//          T:::::T        r::::rrr:::::::::ry:::::y         y:::::y        a::::::::::::a   g:::::::::ggg::::g a::::::::::::a  i:::::in:::nn::::::::nn  ");
+				System.out.println("//          T:::::T        r:::::::::::::::::ry:::::y       y:::::y         aaaaaaaaa:::::a g:::::::::::::::::g aaaaaaaaa:::::a  i::::in::::::::::::::nn ");
+				System.out.println("//          T:::::T        rr::::::rrrrr::::::ry:::::y     y:::::y                   a::::ag::::::ggggg::::::gg          a::::a  i::::inn:::::::::::::::n");
+				System.out.println("//          T:::::T         r:::::r     r:::::r y:::::y   y:::::y             aaaaaaa:::::ag:::::g     g:::::g    aaaaaaa:::::a  i::::i  n:::::nnnn:::::n");
+				System.out.println("//          T:::::T         r:::::r     rrrrrrr  y:::::y y:::::y            aa::::::::::::ag:::::g     g:::::g  aa::::::::::::a  i::::i  n::::n    n::::n");
+				System.out.println("//          T:::::T         r:::::r               y:::::y:::::y            a::::aaaa::::::ag:::::g     g:::::g a::::aaaa::::::a  i::::i  n::::n    n::::n");
+				System.out.println("//          T:::::T         r:::::r                y:::::::::y            a::::a    a:::::ag::::::g    g:::::ga::::a    a:::::a  i::::i  n::::n    n::::n");
+				System.out.println("//        TT:::::::TT       r:::::r                 y:::::::y             a::::a    a:::::ag:::::::ggggg:::::ga::::a    a:::::a i::::::i n::::n    n::::n");
+				System.out.println("//        T:::::::::T       r:::::r                  y:::::y              a:::::aaaa::::::a g::::::::::::::::ga:::::aaaa::::::a i::::::i n::::n    n::::n");
+				System.out.println("//        T:::::::::T       r:::::r                 y:::::y                a::::::::::aa:::a gg::::::::::::::g a::::::::::aa:::ai::::::i n::::n    n::::n");
+				System.out.println("//        TTTTTTTTTTT       rrrrrrr                y:::::y                  aaaaaaaaaa  aaaa   gggggggg::::::g  aaaaaaaaaa  aaaaiiiiiiii nnnnnn    nnnnnn");
+				System.out.println("//                                                y:::::y                                              g:::::g                                           ");
+				System.out.println("//                                               y:::::y                                   gggggg      g:::::g                                           ");
+				System.out.println("//                                              y:::::y                                    g:::::gg   gg:::::g                                           ");
+				System.out.println("//                                             y:::::y                                      g::::::ggg:::::::g                                           ");
+				System.out.println("//                                            yyyyyyy                                        gg:::::::::::::g                                            ");
+				System.out.println("//                                                                                             ggg::::::ggg                                              ");
+				System.out.println("//                                                                                                gggggg                                                 ");
+				System.out.println("");
+				System.out.println("");
+				System.out.println("");
+				System.out.println("");
+				System.out.println("");
+				System.out.println("");
 				System.out.println("                                                                     ");
 
 				Thread.sleep(2500l);
 			}
 
 			System.out.println("                                                                                   ");
-			System.out.println("███████╗███╗   ██╗██████╗ ███████╗██████╗ ");
-			System.out.println("██╔════╝████╗  ██║██╔══██╗██╔════╝██╔══██╗");
-			System.out.println("█████╗  ██╔██╗ ██║██║  ██║█████╗  ██║  ██║");
-			System.out.println("██╔══╝  ██║╚██╗██║██║  ██║██╔══╝  ██║  ██║");
-			System.out.println("███████╗██║ ╚████║██████╔╝███████╗██████╔╝");
-			System.out.println("╚══════╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═════╝ ");
+			System.out.println("//  d sss   d s  b d ss    d sss   d ss    ");
+			System.out.println("//  S       S  S S S   ~o  S       S   ~o  ");
+			System.out.println("//  S       S   SS S     b S       S     b ");
+			System.out.println("//  S sSSs  S    S S     S S sSSs  S     S ");
+			System.out.println("//  S       S    S S     P S       S     P ");
+			System.out.println("//  S       S    S S    S  S       S    S  ");
+			System.out.println("//  P sSSss P    P P sss   P sSSss P sss   ");
 			System.out.println("                                           ");
 		}
 
